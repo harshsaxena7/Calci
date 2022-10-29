a9fe0b880e66089a2af71e90ea581ca35f94bc17
harsh saxena <kinshuk26harsh@gmail.com>
2022-10-29 18:43:38 +0530

    added javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function show(val)
+{
+document.getElementById("calc").value+=val
+ }
+
+function solve()
+{
+let x = document.getElementById("calc").value
+let y = eval(x)
+document.getElementById("calc").value = y
+}
+
+function clr()
+{
+document.getElementById("calc").value = ""
+}
